8bbf62a455a444a0d8838d45f21e806ad3fae864
redhiker <LarryIrvine@verizon.net>
2016-08-05 13:05:44 -0400

    Larry: add login html and js

---
+++ src/main/webapp/login.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+function validateLogin(name,password) {
+	
+	// Add the details (AJAX call)
+	
+	return true; // return true or false
+}
+
+function checkLoginCredentials() {
+	
+	var loginName = document.getElementById('userName').value;
+	
+	var loginPassword = document.getElementById('userPassword').value;
+	
+	// Validate the login
+	
+	var isLoginSuccessful = validateLogin(loginName,loginPassword);
+	
+	if (isLoginSuccessful === true) {
+		
+	} else { // user name or password not recognized
+		
+		
+		
+	}	
+}
+
+function createNewUserAccount() {
+	
+	// Add AJAX call
+	
+}
